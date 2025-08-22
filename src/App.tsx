f5c9c8aafa9d24b3780eb4f1d9361f60bdee26f4
tanisnus <taniss246@gmail.com>
2025-08-21 22:00:16 -0700

    Set Up Project

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <Router>
+
+      {/* Section (Header) */}
+        <section>
+
+        </section>
+
+
+      {/* Section (Main) */}
+        <section>
+          <Routes>
+
+          </Routes>
+
+        </section>
+
+
+      </Router>
+    </>
+  )
+}
+
+export default App
